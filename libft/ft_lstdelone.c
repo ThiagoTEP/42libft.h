e2b6f4a1ded34bdc4434b3813ecf0634e9c487d6
Thiago Evaristo De Paula <thevaris@c2r5s11.42porto.com>
2025-04-21 19:55:36 +0100

    correcao do main e melhoria

--- libft/ft_lstdelone.c
+++ libft/ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: thevaris <thevaris@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 15:48:18 by thevaris          #+#    #+#             */
-/*   Updated: 2025/04/13 18:57:19 by thevaris         ###   ########.fr       */
+/*   Updated: 2025/04/21 19:47:19 by thevaris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,3 +17,17 @@ void	ft_lstdelone(t_list *lst, void (*del)(void*))
 	del((lst)->content);
 	free(lst);
 }
+/*
+#include <stdio.h>
+int main() {
+    
+    t_list *node = ft_lstnew(ft_strdup("Node to delete"));
+
+    ft_lstdelone(node, free);
+
+    // Verificação após deletar (não deve haver nada impresso)
+    if (!node)
+        printf("Node was deleted!\n");
+
+    return 0;
+}*/
--- libft/ft_lstsize.c
+++ libft/ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: thevaris <thevaris@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 15:39:36 by thevaris          #+#    #+#             */
-/*   Updated: 2025/04/13 19:02:13 by thevaris         ###   ########.fr       */
+/*   Updated: 2025/04/21 19:54:10 by thevaris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,3 +26,21 @@ int	ft_lstsize(t_list *lst)
 	}
 	return (size);
 }
+/*
+#include <stdio.h>
+int main(void)
+{
+	t_list *lista = NULL;
+
+	ft_lstadd_back(&lista, ft_lstnew(ft_strdup("primeiro")));
+	ft_lstadd_back(&lista, ft_lstnew(ft_strdup("segundo")));
+	ft_lstadd_back(&lista, ft_lstnew(ft_strdup("terceiro")));
+
+	int tamanho = ft_lstsize(lista);
+
+	printf("Tamanho da lista: %d\n", tamanho);
+
+	ft_lstclear(&lista, free);
+
+	return 0;
+}*/
